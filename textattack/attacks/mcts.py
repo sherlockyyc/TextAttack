f941fe7631c6933ca6e3acbbbdca2d46513dad8c
Bill Zhang <bzh0807@gmail.com>
2019-11-01 16:04:50 -0400

    use fields instead of fcn params

--- textattack/attacks/mcts.py
+++ textattack/attacks/mcts.py
@@ -68,31 +68,31 @@ class MCTS(Attack):
         node.lrave_variance[fi] = math.sqrt( ((reward_V - node.lrave_score[fi]) * (reward_V - lrave_score_pre) + node.lrave_count[fi] * node.lrave_variance[fi] * node.lrave_variance[fi])/(node.lrave_count[fi]+1))
         node.lrave_count[fi] = node.lrave_count[fi] + 1
 
-    def update_gRAVE(self, gRAVE, F, reward_V ):
+    def update_gRAVE(self, F, reward_V):
         # update gRAVE score for each feature of feature subset F, by adding the reward_V the the score
-        for fi in range(gRAVE[0].shape[0]):
+        for fi in range(self.gRAVE[0].shape[0]):
             if F[fi]:
-                gRAVE[0][fi] = (gRAVE[0][fi]*gRAVE[1][fi] + reward_V)/(gRAVE[1][fi] + 1)
-                gRAVE[1][fi] += 1 
+                self.gRAVE[0][fi] = (self.gRAVE[0][fi]*self.gRAVE[1][fi] + reward_V)/(self.gRAVE[1][fi] + 1)
+                self.gRAVE[1][fi] += 1 
 
 
-    def UCB(self, node, gRAVE, params):
+    def UCB(self, node):
     # b<1
         d = len(node.allowed_features)  # feature subset size
         f = node.feature_set.shape[0] # number of features
-        nrand = params.nrand
+        nrand = self.params.nrand
 
         if (node.T_f < nrand): # we perform random exploration fort the first 50 visits
             return -1 #//-1 indicate that we don't to want chose any feature
 
-        if (pow((node.T_f+1),params.b)-pow(node.T_f,params.b)>1):
+        if (pow((node.T_f+1), self.params.b)-pow(node.T_f, self.params.b)>1):
             if not node.allowed_features:
                 return -1
             else:
                 for ft in range(f):
                     if not ft in allowed_features:
-                        beta = params.cl/(params.cl + node.lrave_count[ft])
-                        rst = (1-beta) * lrave_score[ft] + beta * gRAVE[0][ft]
+                        beta = self.params.cl/(self.params.cl + node.lrave_count[ft])
+                        rst = (1-beta) * self.lrave_score[ft] + beta * self.gRAVE[0][ft]
                         if rst>nowbest:
                             ft_now = ft
                             nowbest = rst
@@ -110,42 +110,42 @@ class MCTS(Attack):
         return UCB_max_feature
 
 
-    def update_Tree_And_Get_Address(self, tree, current_features, params):
-        if not tree.find(current_features):
+    def update_Tree_And_Get_Address(self, current_features):
+        if not self.tree.find(current_features):
             node = Node(current_features)
-            tree.save(current_features, node)
+            self.tree.save(current_features, node)
         else:
-            node = tree.find(current_features)
+            node = self.tree.find(current_features)
         return node
 
-    def iterate(self, tree, gRAVE, current_features, params, depth):
-        if depth>=params.max_depth:
-            reward_V = params.get_reward(current_features, params)
-            self.update_gRAVE(gRAVE, current_features, reward_V)
+    def iterate(self, current_features, depth):
+        if depth>=self.params.max_depth:
+            reward_V = self.params.get_reward(current_features)
+            self.update_gRAVE(current_features, reward_V)
         else:
-            next_node = self.update_Tree_And_Get_Address(tree, current_features, params)
+            next_node = self.update_Tree_And_Get_Address(current_features)
 
             if (next_node.T_f != 0):
-                fi = self.UCB(next_node, gRAVE, params)
+                fi = self.UCB(next_node)
                 if (fi==-1): # it means that no feature has been selected and that we are going to perform random exploration
                     depth = current_features.sum()
-                    reward_V = self.iterate_random(tree, current_features, params)
-                    self.update_gRAVE(gRAVE, current_features, reward_V)
+                    reward_V = self.iterate_random(self.tree, current_features)
+                    self.update_gRAVE(current_features, reward_V)
                 else: #add the feature to the feature set
                     current_features[fi] = True
-                    reward_V = self.iterate(tree,gRAVE,current_features,params,depth+1)
+                    reward_V = self.iterate(current_features, depth+1)
             else:
                 depth_now = current_features.sum()
-                reward_V = self.iterate_random(tree, current_features, params)
-                self.update_gRAVE(gRAVE, current_features, reward_V)
+                reward_V = self.iterate_random(self.tree, current_features)
+                self.update_gRAVE(current_features, reward_V)
                 fi = -1 # indicate that random exploration has been perform and thus no feature selected
             self.update_Node(next_node, fi, current_features, reward_V)
         return reward_V
 
-    def iterate_random(self, tree, current_features, params):
+    def iterate_random(self, tree, current_features):
         f_num = current_features.shape[0]
         f_size  = current_features.sum()
-        while (f_size < params.max_depth):
+        while (f_size < self.params.max_depth):
             if (f_num<=f_size):
                 break
             #chose a random feature that is not already in the feature subset, and put its value to one (and not the stopping feature)
@@ -160,9 +160,9 @@ class MCTS(Attack):
             current_features[it] = True
             f_size += 1
 
-        return params.get_reward(self, current_features, params)
+        return self.params.get_reward(self, current_features)
 
-    def reward(self, f, params):
+    def reward(self, f):
         #global bst
         score = 0
         for i in range(f.shape[0]):
@@ -188,10 +188,10 @@ class MCTS(Attack):
     def runmcts(self):
         for i in range(self.lcount):
             current_features = np.array([False] * self.nsize, dtype = bool)
-            self.iterate(self.tree, self.gRAVE, current_features, self.params, 0)  
+            self.iterate(current_features, 0)  
 
     
 if __name__  == '__main__':
-    m_attk = MCTS(None, MCTS.reward, 10, 100000, 100)
+    m_attk = MCTS(None, MCTS.reward, 10, 100000, 100) #params: model, rewardfunc, maxdepth, lcount, nsize
     m_attk.runmcts()
     print(m_attk.gRAVE)
